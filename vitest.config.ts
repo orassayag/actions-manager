365b229441d230b4d37aae7f180fa41f2a9f6bde
Or Assayag <orassayag@gmail.com>
2026-05-08 22:12:38 +0300

    fix: Fix the vitest.config.ts file

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,11 +21,12 @@ export default defineConfig({
         '**/*.config.*',
       ],
       include: ['src/**/*.ts'],
-      all: true,
-      lines: 80,
-      functions: 80,
-      branches: 80,
-      statements: 80,
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
     },
   },
 });
